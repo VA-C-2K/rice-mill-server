601359fe970ca574e734fb4a1cc460acfa69841b
Viraj Agharkar <viraj@quolam.co>
2023-08-27 20:59:49 +0530

    changes for prod

--- src/server.js
+++ src/server.js
@@ -11,20 +11,29 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 // eslint-disable-next-line unused-imports/no-unused-vars
 const colors = require("colors");
-const path = require("path");
 
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:5173"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+if (process.env.NODE_ENV === "prod") {
+  app.use(
+    cors({
+      origin: ["http://127.0.0.1:5173"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
+} else {
+  app.use(
+    cors({
+      origin: [process.env.PROD_CLIENT_URL],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
+}
 
 app.use("/user", userRoutes);
 app.use("/customer", customerRoutes);
@@ -32,17 +41,11 @@ app.use("/vendor", vendorRoutes);
 app.use("/product", productRoutes);
 app.use("/employee", employeeRoutes);
 
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "prod") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running");
-  });
-}
+
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 // Error Handling middlewares
 app.use(notFound);
