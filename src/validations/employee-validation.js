2beb785d4ddb35b277123d50481dddab4ccc38b8
Viraj Agharkar <viraj@quolam.com>
2024-08-19 00:28:28 +0530

    formatting

--- src/validations/employee-validation.js
+++ src/validations/employee-validation.js
@@ -1,26 +1,30 @@
 import Joi from "joi";
 import { validatePayload } from "./index.js";
 
-export const createEmployeeSchemaValidation = validatePayload(Joi.object({
-  phone_number: Joi.string().required(),
-  first_name: Joi.string().required(),
-  last_name: Joi.string().required(),
-  address: Joi.string(),
-  salary: Joi.number().required(),
-  aadhar_card_no: Joi.string(),
-  no_of_leaves: Joi.number(),
-  role: Joi.string().required(),
-  over_time_hrs: Joi.number(),
-}));
+export const createEmployeeSchemaValidation = validatePayload(
+  Joi.object({
+    phone_number: Joi.string().required(),
+    first_name: Joi.string().required(),
+    last_name: Joi.string().required(),
+    address: Joi.string(),
+    salary: Joi.number().required(),
+    aadhar_card_no: Joi.string(),
+    no_of_leaves: Joi.number(),
+    role: Joi.string().required(),
+    over_time_hrs: Joi.number(),
+  })
+);
 
-export const updateEmployeeSchemaValidation = validatePayload(Joi.object({
-  phone_number: Joi.string(),
-  first_name: Joi.string(),
-  last_name: Joi.string(),
-  address: Joi.string(),
-  salary: Joi.number(),
-  aadhar_card_no: Joi.string(),
-  no_of_leaves: Joi.number(),
-  role: Joi.string(),
-  over_time_hrs: Joi.number(),
-}).min(1));
+export const updateEmployeeSchemaValidation = validatePayload(
+  Joi.object({
+    phone_number: Joi.string(),
+    first_name: Joi.string(),
+    last_name: Joi.string(),
+    address: Joi.string(),
+    salary: Joi.number(),
+    aadhar_card_no: Joi.string(),
+    no_of_leaves: Joi.number(),
+    role: Joi.string(),
+    over_time_hrs: Joi.number(),
+  }).min(1)
+);
